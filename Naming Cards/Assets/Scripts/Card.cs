fef2efe87ba31d2c75a80169f5d195388f472072
Vitor Santana Cordeiro <vitorsanusp@gmail.com>
2019-06-04 14:40:05 -0300

    Upload Unity project

---
+++ Naming Cards/Assets/Scripts/Card.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Card", menuName = "Card")]
+public class Card : ScriptableObject
+{
+	public Sprite figure;
+	public new string name;
+	public int streak = 0;
+	public int delay = 0;
+}
